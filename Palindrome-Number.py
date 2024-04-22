bb32eac323e7e4e10b53603674200299848c8212
Dylan McDougall <dylan.7mcdougall@gmail.com>
2024-04-22 10:30:11 -0700

    Create Palindrome-Number.py

---
+++ Palindrome-Number.py
@@ -0,0 +1,5 @@
+# Time to complete: 7:38 (got caught up in a silly approach trying to reverse up to the midpoint instead of the entire converted string)
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        return str(x) == str(x)[::-1]
