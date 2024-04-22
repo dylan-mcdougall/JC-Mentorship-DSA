11c77fb36558d2a37d18335807e57cbbd06897b3
Dylan McDougall <dylan.7mcdougall@gmail.com>
2024-04-22 10:31:38 -0700

    Update Palindrome-Number.py

--- Palindrome-Number.py
+++ Palindrome-Number.py
@@ -1,4 +1,4 @@
-# Time to complete: 7:38 (got caught up in a silly approach trying to reverse up to the midpoint instead of the entire converted string)
+# Time to complete: 17:38 (got caught up in a silly approach trying to reverse up to the midpoint instead of the entire converted string)
 
 class Solution:
     def isPalindrome(self, x: int) -> bool:
